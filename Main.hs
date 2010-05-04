c0f04390b40759ebe94a93c23996e92adcaf7833
Joel Lehtonen <joel.lehtonen@iki.fi>
2010-05-05 00:02:20 +0300

    A simple OpenGL 436 byte demo. Contains the packer.

---
+++ Main.hs
@@ -0,0 +1,35 @@
+module Main where
+
+import Graphics.Rendering.OpenGL
+import Graphics.UI.GLUT
+
+main = do 
+  (progname, _) <- getArgsAndInitialize
+  createWindow "436 tavun demo!"
+  displayCallback $= display
+  mainLoop
+
+display = do 
+  clear [ColorBuffer]
+  renderPrimitive Quads $ do
+    color $ (Color3 (1.0::GLfloat) 0 0)
+    vertex $ (Vertex3 (0::GLfloat) 0 0)
+    vertex $ (Vertex3 (0::GLfloat) 0.2 0)
+    vertex $ (Vertex3 (0.2::GLfloat) 0.2 0)
+    vertex $ (Vertex3 (0.2::GLfloat) 0 0)
+    color $ (Color3 (0::GLfloat) 1 0)
+    vertex $ (Vertex3 (0::GLfloat) 0 0)
+    vertex $ (Vertex3 (0::GLfloat) (-0.2) 0)
+    vertex $ (Vertex3 (0.2::GLfloat) (-0.2) 0)
+    vertex $ (Vertex3 (0.2::GLfloat) 0 0)
+    color $ (Color3 (0::GLfloat) 0 1)
+    vertex $ (Vertex3 (0::GLfloat) 0 0)
+    vertex $ (Vertex3 (0::GLfloat) (-0.2) 0)
+    vertex $ (Vertex3 ((-0.2)::GLfloat) (-0.2) 0)
+    vertex $ (Vertex3 ((-0.2)::GLfloat) 0 0)
+    color $ (Color3 (1::GLfloat) 0 1)
+    vertex $ (Vertex3 (0::GLfloat) 0 0)
+    vertex $ (Vertex3 (0::GLfloat) 0.2 0)
+    vertex $ (Vertex3 ((-0.2::GLfloat)) 0.2 0)
+    vertex $ (Vertex3 ((-0.2::GLfloat)) 0 0)
+  flush
